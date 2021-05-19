326e61dcfea98a856b0f73f5d58bb877d38b299c
GoPavel0202 <84277237+GoPavel0202@users.noreply.github.com>
2021-05-19 19:54:39 +0300

    Add files via upload

---
+++ homework_three.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+
+
+int main(int argc, const char** argv) {//
+//3.1
+extern const int a;
+extern const int b;
+extern const double c;
+extern const int d;
+double i =  a * (b + (c / d));
+std::cout << i << std::endl;
+//3.2
+int x = 25;
+const int s = 21;
+int f = x - s;
+std::cout << f << std::endl;
+
+int i5 = (x > s) ? (f * 2) : 0;
+std::cout << i5<< std::endl;
+//3.3
+
+//3.4
+short field[3][3][3] = {};
+	field[0][0][0] = 1;
+	field[1][0][0] = 2;
+	field[2][0][0] = 3;
+	field[1][1][1] = 200;
+	short* e = &field[1][1][1];
+	
+	std::cout <<  field[0][0][0] << " ";
+	std::cout <<  field[1][0][0] << " ";
+	std::cout <<  field[2][0][0] << " ";
+	std::cout << field[1][1][1] << std::endl;
+	std::cout << * (field +1 +1 +1 ) << std::endl;
+	printf("%p\n", e);
+    printf("%d\n", *e);
+	return 0;
+}
+	
---
+++ homework_three_one.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+
+extern const int a = 2;
+extern const int b = 9;
+extern const double c = 40;
+extern const int d = 6;
